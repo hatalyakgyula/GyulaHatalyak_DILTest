8e536c07c064c77827db569530d9aefb8becf4e9
Gyula Hatalyak <gyula.hatalyak@cognex.com>
2023-09-15 15:52:05 +0200

    Add GhostDrawing app.

---
+++ Part2/GhostDrawing/GhostDrawing/GhostDrawingApp.swift
@@ -0,0 +1,18 @@
+//
+//  GhostDrawingApp.swift
+//  GhostDrawing
+//
+//  Created by Gyula Hatalyak on 2023. 09. 09..
+//
+
+import SwiftUI
+
+@main
+struct GhostDrawingApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
